e94424b583bc844951e1025a0ee77a0a3e9973ea
Chobby <46777310+kwb020312@users.noreply.github.com>
2023-10-02 23:11:29 +0900

    Feat: Raycasting

--- src/scene.js
+++ src/scene.js
@@ -13,6 +13,9 @@ export function createScene() {
   renderer.setSize(gameWindow.offsetWidth, gameWindow.offsetHeight);
   gameWindow.appendChild(renderer.domElement);
 
+  const raycaster = new THREE.Raycaster()
+  const mouse = new THREE.Vector2()
+  let selectedObject = undefined
   let terrain = [];
   let buildings = [];
 
@@ -88,6 +91,17 @@ export function createScene() {
 
   function onMouseDown(event) {
     camera.onMouseDown(event);
+
+    mouse.x = (event.clientX / renderer.domElement.clientWidth) * 2 - 1
+    mouse.y = -(event.clienty / renderer.domElement.clientHeight) * 2 + 1
+
+    raycaster.setFromCamera(mouse, camera.camera)
+
+    let intersections = raycaster.intersectObjects(scene.children, false)
+
+    if(intersections.length > 0) {
+      console.log(intersections[0])
+    }
   }
 
   function onMouseUp(event) {
