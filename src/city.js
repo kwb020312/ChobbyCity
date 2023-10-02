0d628318949cd9e560fe47f4ee3bb03ce2a09036
김우빈 <kwb020312@naver.com>
2023-10-02 12:43:04 +0900

    Feat: Add createCity Method

---
+++ src/city.js
@@ -0,0 +1,22 @@
+export function createCity(size) {
+  const data = [];
+
+  initialize();
+
+  //   N * N 의 데이터 생성
+  function initialize() {
+    for (let x = 0; x < size; x++) {
+      const column = [];
+      for (let y = 0; y < size; y++) {
+        const tile = { x, y };
+        column.push(tile);
+      }
+      data.push(column);
+    }
+  }
+
+  return {
+    size,
+    data,
+  };
+}
--- src/scene.js
+++ src/scene.js
@@ -12,10 +12,27 @@ export function createScene() {
   renderer.setSize(gameWindow.offsetWidth, gameWindow.offsetHeight);
   gameWindow.appendChild(renderer.domElement);
 
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-  const mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
+  let meshes = [];
+
+  function initialize(city) {
+    scene.clear();
+    meshes = [];
+    for (let x = 0; x < city.size; x++) {
+      const column = [];
+      for (let y = 0; y < city.size; y++) {
+        // 1. 각 좌표에 해당하는 메시/3D 객체를 불러온다.
+        // 2. scene에 mesh를 추가한다.
+        const geometry = new THREE.BoxGeometry(1, 1, 1);
+        const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+        const mesh = new THREE.Mesh(geometry, material);
+        mesh.position.set(x, 0, y);
+        scene.add(mesh);
+        column.push(mesh);
+      }
+
+      meshes.push(column);
+    }
+  }
 
   function draw() {
     renderer.render(scene, camera.camera);
@@ -42,6 +59,7 @@ export function createScene() {
   }
 
   return {
+    initialize,
     start,
     stop,
     onMouseDown,
