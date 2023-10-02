5377d49ebbf96f3b271191f9c141acf23543a08b
김우빈 <kwb020312@naver.com>
2023-10-02 21:54:34 +0900

    Refactor: Tile Creation

--- src/city.js
+++ src/city.js
@@ -8,23 +8,7 @@ export function createCity(size) {
     for (let x = 0; x < size; x++) {
       const column = [];
       for (let y = 0; y < size; y++) {
-        const tile = {
-          x,
-          y,
-          buildingId: undefined,
-          update() {
-            const x = Math.random();
-            if (x < 0.01) {
-              if (this.buildingId === undefined) {
-                this.buildingId = "building-1";
-              } else if (this.buildingId === "building-1") {
-                this.buildingId = "building-2";
-              } else if (this.buildingId === "building-2") {
-                this.buildingId = "building-3";
-              }
-            }
-          },
-        };
+        const tile = createTile(x, y);
 
         column.push(tile);
       }
@@ -46,5 +30,26 @@ export function createCity(size) {
     size,
     data,
     update,
+  };
+}
+
+function createTile(x, y) {
+  return {
+    x,
+    y,
+    terrainId: "grass",
+    buildingId: undefined,
+    update() {
+      const x = Math.random();
+      if (x < 0.01) {
+        if (this.buildingId === undefined) {
+          this.buildingId = "building-1";
+        } else if (this.buildingId === "building-1") {
+          this.buildingId = "building-2";
+        } else if (this.buildingId === "building-2") {
+          this.buildingId = "building-3";
+        }
+      }
+    },
   };
 }
